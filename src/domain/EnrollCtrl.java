fe02d33fbd85f260adf3d7400127c347ff9cbe80
Sahar Shirmardi <70791885+sahar-shirmardi@users.noreply.github.com>
2022-06-17 22:06:30 +0430

    Add duplicate enrollment request check method

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -7,40 +7,52 @@ import domain.exceptions.EnrollmentRulesViolationException;
 import static domain.CSE.getUnitsRequested;
 
 public class EnrollCtrl {
-	public static void enroll(Student s, List<CSE> courses) throws EnrollmentRulesViolationException {
-		Map<Term, Map<Course, Double>> transcript = s.getTranscript();
-		for (CSE o : courses) {
-			
-		    for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
-			for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
-			    if (r.getKey().equals(o.getCourse()) && r.getValue() >= 10)
-				throw new EnrollmentRulesViolationException(String.format("The student has already passed %s", o.getCourse().getName()));
-			}
-		    }
-		    List<Course> prereqs = o.getCourse().getPrerequisites();
-		    nextPre:
-		    for (Course pre : prereqs) {
-			for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
-			    for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
-				if (r.getKey().equals(pre) && r.getValue() >= 10)
-				    continue nextPre;
-			    }
-			}
-			throw new EnrollmentRulesViolationException(String.format("The student has not passed %s as a prerequisite of %s", pre.getName(), o.getCourse().getName()));
-		    }
+    public static void enroll(Student s, List<CSE> courses) throws EnrollmentRulesViolationException {
+        Map<Term, Map<Course, Double>> transcript = s.getTranscript();
+        for (CSE o : courses) {
+            for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
+                for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
+                    if (r.getKey().equals(o.getCourse()) && r.getValue() >= 10)
+                        throw new EnrollmentRulesViolationException(String.format("The student has already passed %s", o.getCourse().getName()));
+                }
+            }
+            List<Course> prereqs = o.getCourse().getPrerequisites();
+            nextPre:
+            for (Course pre : prereqs) {
+                for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
+                    for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
+                        if (r.getKey().equals(pre) && r.getValue() >= 10)
+                            continue nextPre;
+                    }
+                }
+                throw new EnrollmentRulesViolationException(String.format("The student has not passed %s as a prerequisite of %s", pre.getName(), o.getCourse().getName()));
+            }
+        }
+        checkDuplicateEnrollRequest(courses);
+        for (CSE o : courses) {
             for (CSE o2 : courses) {
                 if (o == o2)
                     continue;
                 if (o.getExamTime().equals(o2.getExamTime()))
                     throw new EnrollmentRulesViolationException(String.format("Two offerings %s and %s have the same exam time", o, o2));
-                if (o.getCourse().equals(o2.getCourse()))
-                    throw new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice", o.getCourse().getName()));
             }
-	}
+        }
         checkUnitsLimit(s, courses);
         for (CSE o : courses)
             s.takeCourse(o.getCourse(), o.getSection());
     }
+
+    private static void checkDuplicateEnrollRequest(List<CSE> courses) throws EnrollmentRulesViolationException {
+        for (CSE o : courses) {
+            for (CSE o2 : courses) {
+                if (o == o2)
+                    continue;
+                if (o.getCourse().equals(o2.getCourse()))
+                    throw new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice", o.getCourse().getName()));
+            }
+        }
+    }
+
     private static void checkUnitsLimit(Student s, List<CSE> courses) throws EnrollmentRulesViolationException {
         if ((s.getGPA() < 12 && getUnitsRequested(courses) > 14) ||
                 (s.getGPA() < 16 && getUnitsRequested(courses) > 16) ||
